ba573999f316143e83a70d3987d9bd354c351d5b
Aleksej Ignatenko <ignatenkoaleksej771@gmail.com>
2024-12-07 20:24:49 +0300

    Изменено SupplierDto.

--- Dto/SupplierDto.cs
+++ Dto/SupplierDto.cs
@@ -4,6 +4,5 @@
     {
         public Guid Id { get; set; }
         public string SupplierName { get; set; } = string.Empty;
-        public List<Guid> SupplierIds { get; set; } = new List<Guid>();
     }
 }
